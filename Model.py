6cc2c05490a73b57a4e543344dfd6469b3ac560e
shotni4k <shotni4k@gmail.com>
2022-02-06 23:44:45 +0600

    create ignore-file

--- Model.py
+++ Model.py
@@ -1,56 +1,46 @@
 import json
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from error_handler.ErrorHandler import ErrorHandler
 from colorama import Back,Fore
 
 
-#(
-#  1 - написать функцию для тестирования модели  
-# )
-
-
-class Model():
+class  LogisticRegressionModel():
     """ Класс Модели машинного обучения написанной на основе LogisticRegression() """
     def __init__(self):
         self.vectorizer = CountVectorizer()
         self.classifier =  LogisticRegression()
+        self.config = json.load(open(('jsonfile/BOT_CONFIG')))
 
 
-    def GetDataSet(self):
+    def get_data_set(self):
         """ Метод разбивающий dataset на тренировочный и тестовый"""
-        from sklearn.model_selection import train_test_split
-    
-        X = []
+        x = []
         y = []
 
-        try:
-            with open('jsonfile/BOT_CONFIG.json',encoding='utf-8') as file:
-                config = json.load(file)
-        except FileNotFoundError as Error:
-            from error_handler.ErrorHandler import SaveErrors
-            print(Back.WHITE,Fore.RED,"Отсутсвует Json-file")
-            SaveErrors(Error)
-            
-        for intent in config['intents'].keys():
-            for example in config['intents'][intent]["examples"]:
-                X.append(example),y.append(intent)
+        for intent in self.config['intents'].keys():
+            for example in self.config['intents'][intent]["examples"]:
+                x.append(example),y.append(intent)
 
-        train_test_split(X,y)
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-        self.ModelTestTrainig(X_test, y_test)
-        return  self.ModelTraining(X_train,y_train)
+        train_test_split(x,y)
+        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+        self.model_test_trainig(X_test, y_test)
+
+        return  self.model_training(X_train,y_train)
     
 
-    def ModelTraining(self,X_train,y_train):
+    def model_training(self,X_train,y_train):
         """ Метод обучающий модель  """
         # создание векторов
         self.vectorizer.fit(X_train)
         X_vectors = self.vectorizer.fit_transform(X_train)
         # обучение класификатора 
         self.classifier.fit(X_vectors,y_train)
+
       
         
-    def ModelTestTrainig(self,X_test,y_test):
+    def model_test_trainig(self,X_test,y_test):
         """ Метод тестирующий модель """
         # Создание векторов
         self.vectorizer.fit(X_test)
@@ -59,21 +49,17 @@ class Model():
         self.classifier.fit(X_VectorsTest,y_test)
         return print(Fore.GREEN,f"Примерная точность модели {self.classifier.score(X_VectorsTest, y_test)}") 
 
-    def GetIntentByModel(self,text):
+    def get_intent_by_model(self,text):
         """ Метод возвращающий интент  """
         return self.classifier.predict(self.vectorizer.transform([text]))
         
 
 
 
-model = Model()
-def start():
-    model.GetDataSet()
+model = LogisticRegressionModel()
+error_handler = ErrorHandler()
+try:
+    model.get_data_set()
+except FileNotFoundError as error:
+    error_handler.save_errors(error)
 
-while True:
-    try:
-        start()
-        text = input("Напиши текст \n")
-        print(model.GetIntentByModel(text))
-    finally:
-        print("Приложкние завершило работу")
