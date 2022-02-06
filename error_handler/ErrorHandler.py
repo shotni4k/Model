ae66a8f0ff035bd8c80b469b9ccf7ec2bd6a98a5
shotni4k <shotni4k@gmail.com>
2022-02-06 23:47:59 +0600

    create ignore-file

--- error_handler/ErrorHandler.py
+++ error_handler/ErrorHandler.py
@@ -1,49 +1,45 @@
-def SaveErrorsEmail(text):
-    """ Функция отпровляющая сообщение об ошибке на почту"""
-    #(
-    # 1- не ктотырые вещи можно вынести в списки
-    # )
-    import smtplib   
-    from email.mime.multipart import MIMEMultipart      # Многокомпонентный объект
-    from email.mime.text import MIMEText                # Текст/HTML
-    from email.mime.image import MIMEImage  
+import json
+import smtplib   
+from datetime import datetime
+from email.mime.multipart import MIMEMultipart      
+from email.mime.text import MIMEText                
+from email.mime.image import MIMEImage  
 
-    addr_from = 'sim76697@gmail.com' # Адресат 
-    addr_to  = 'shotni4k@gmail.com'  # Получатель
-    password = '7_JPhAjppYT.YBS'  # Пороль
+class ErrorHandler():
+    def __init__(self):
+        self.conig_data = json.load(open(('JsonFile/config.json')))
+        self.server = smtplib.SMTP('smtp.gmail.com', 587)
+        self.server.ehlo()
+        self.server.starttls()
+        self.server.ehlo()    
+        self.server.login(self.conig_data["addr_from"],self.conig_data["password"])
     
-    msg = MIMEMultipart()
-    msg['From'] = addr_from
-    msg['To'] = addr_to    
-    msg['Subject'] = 'Ошибка у бота '  # Тема сообщения
-    body = f'ошибка {text[0]}' # Техт сообщения 
-    msg.attach(MIMEText(body, 'palin'))
+    def send_message(self,text):
+        msg = MIMEMultipart()
+        msg['From'] = self.conig_data["addr_from"]
+        msg['To'] = self.conig_data["addr_to"]
+        msg['Subject'] = 'Ошибка у бота '  # Тема сообщения
+        body = f'ошибка {text[0]}' # Техт сообщения 
+        msg.attach(MIMEText(body, 'palin'))
 
-    server = smtplib.SMTP('smtp.gmail.com', 587) # обьект SMPT
-    server.set_debuglevel(False) 
-    server.starttls() # Начинаем шифрованный обмен по TLS
-    server.login(addr_from,password) # Авторизация
-    server.send_message(msg) # Отправка сообщения 
-    server.quit()
-    return 
+        return self.server.send_message(msg)
 
-def SaveErrors(Error):
-    import json
-    from datetime import datetime
-    
-    
-    text =[]
-    text.append({
-                    'error': Error,
+  
+
+    def save_errors(self,error):
+        text =[]
+        text.append({
+                    'error': error,
                     'data': datetime.now().date(),
                     'time': datetime.now().time(),
                  },)
-    with open('errors/errors.json','w',encoding= 'utf-8') as errors:
-          errors.write(json.dumps(  text,
-                                    ensure_ascii = False,
-                                    indent = 4,
-                                    sort_keys= True,
-                                    default= str
+        with open('JsonFile/errors.json','w',encoding= 'utf-8') as errors:
+            errors.write(json.dumps(  text,
+                                      ensure_ascii = False,
+                                      indent = 4,
+                                      sort_keys= True,
+                                      default= str
                                     ))
 
-    return save_errors_email(text)
+        return self.send_message(text)
+
