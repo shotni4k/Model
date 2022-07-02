299689847d41a5fbc711c8ced87e29c1b68b25a1
shotni4k <shotni4k@gmail.com>
2022-07-02 18:05:40 +0600

    update model

--- Model.py
+++ Model.py
@@ -1,65 +1,61 @@
 import json
-from sklearn.feature_extraction.text import CountVectorizer
+from loguru import logger
+from numpy import ndarray
+from dataclasses import dataclass
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
 from error_handler.ErrorHandler import ErrorHandler
-from colorama import Fore
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
 
-error_handler = ErrorHandler()
+@dataclass(frozen=True)
+class Test_set():
+    X_test: list
+    Y_test: list
+
+@dataclass(frozen=True)
+class DataSet():
+    X_data: list
+    Y_data: list
 
 
-class  LogisticRegressionModel():
+class LogisticRegressionModel():
     """ Класс Модели машинного обучения написанной на основе LogisticRegression() """
     def __init__(self):
         self.vectorizer = CountVectorizer()
-        self.classifier =  LogisticRegression()
+        self.classifier = LogisticRegression()
         try:
-            self.config = json.load(open(('jsonfile/BOT_CONFIG')))
+            self.data_json = json.load(open('JsonFile/BOT_CONFIG.json'))
         except FileNotFoundError as error:
-            error_handler.save_errors(error)
-
-    def get_data_set(self):
+            logger.error(error)
+    
+    def _get_data_set(self) -> tuple:
         """ Метод разбивающий dataset на тренировочный и тестовый"""
-        x = []
-        y = []
+        X = []
+        Y = []
 
-        for intent in self.config['intents'].keys():
-            for example in self.config['intents'][intent]["examples"]:
-                x.append(example),y.append(intent)
+        for intent in self.data_json['intents'].keys():
+            for example in self.data_json['intents'][intent]["examples"]:
+                X.append(example), Y.append(intent)
 
-        train_test_split(x,y)
-        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
-        self.model_test_trainig(X_test, y_test)
-
-        return  self.model_training(X_train,y_train)
-    
-
-    def model_training(self,X_train,y_train):
-        """ Метод обучающий модель  """
-        # создание векторов
-        self.vectorizer.fit(X_train)
-        X_vectors = self.vectorizer.fit_transform(X_train)
-        # обучение класификатора 
-        self.classifier.fit(X_vectors,y_train)
-
-      
+        X_data,X_test,Y_data,Y_test = train_test_split(X,Y, test_size=0.2)
         
-    def model_test_trainig(self,X_test,y_test):
+        return Test_set(X_test = X_test,Y_test = Y_test),DataSet(X_data =X_data, Y_data= Y_data)
+        
+
+    def model_test_train(self):
         """ Метод тестирующий модель """
-        # Создание векторов
-        self.vectorizer.fit(X_test)
-        X_VectorsTest = self.vectorizer.fit_transform(X_test)
-        # Обучение Класификатора
-        self.classifier.fit(X_VectorsTest,y_test)
-        return print(Fore.GREEN,f"Примерная точность модели {self.classifier.score(X_VectorsTest, y_test)}") 
+        test_set = self._get_data_set()
+        X_VectorsTest = self.vectorizer.fit_transform(test_set[0].X_test)
+        self.classifier.fit(X_VectorsTest,test_set[0].Y_test)
+        return logger.info(f"Приерная точность модели {self.classifier.score(X_VectorsTest, test_set[0].Y_test)}")
 
-    def get_intent_by_model(self,text):
-        """ Метод возвращающий интент  """
-        return self.classifier.predict(self.vectorizer.transform([text]))
+    def model_train(self):
+        """ Метод тестирующий модель """
+        data_set = self._get_data_set()
+        X_Vectors = self.vectorizer.fit_transform(data_set[1].X_data)
+        self.classifier.fit(X_Vectors,data_set[1].Y_data)
+        return logger.info("Модель обученна")
         
-
-
-
-model = LogisticRegressionModel()
-    
-   
+    def get_intent(self,text:str) -> ndarray:
+        """ Метод возвращающий интент"""
+        return self.classifier.predict(self.vectorizer.transform([text]))
